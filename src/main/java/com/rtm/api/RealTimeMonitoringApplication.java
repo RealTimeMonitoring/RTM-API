dbfa4a4992baec263ec23e5ef052f0963b59ad24
Erik <efvergani@hotmail.com>
2024-08-29 21:23:13 -0300

    first commit

---
+++ src/main/java/com/rtm/api/RealTimeMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.rtm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeMonitoringApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeMonitoringApplication.class, args);
+    }
+    
+}
